6b4153509ec1d965458535bdbb864383df73cb05
khalledalkarmi <khalled_95@hotmail.com>
2022-11-26 20:56:54 +0300

    add logout route

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,8 @@ Route::post('/register',[AuthController::class,'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('/logout', [AuthController::class, 'logout']);
+
 
 
 Route::post('/login/callback', [SocialiteController::class, 'handleProviderCallback']);
